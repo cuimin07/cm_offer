9437b91fb2a2857ce53b38c59d3d215f0446c781
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2020-02-27 17:28:11 +0800

    算法题-调整数组顺序使奇数位于偶数前面

---
+++ 21.调整数组顺序使奇数位于偶数前面.py
@@ -0,0 +1,26 @@
+'''
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+
+示例：
+输入：nums = [1,2,3,4]
+输出：[1,3,2,4] 
+注：[3,1,2,4] 也是正确的答案之一。
+
+提示：
+1 <= nums.length <= 50000
+1 <= nums[i] <= 10000
+'''
+
+#答：
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+
+        # odd, even = [], []
+        # for i in nums:
+        #     if i % 2 == 0:
+        #         even.append(i)
+        #     else:
+        #         odd.append(i)
+        # return odd + even
+        # 一行代码
+        return sorted(nums,key=lambda x:1-x%2)
