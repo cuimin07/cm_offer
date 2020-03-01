1c62ac95d343b07f3d327888abd57bfe4ccc1fc5
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2020-03-01 21:50:58 +0800

    算法题-合并两个排序的链表

---
+++ 25.合并两个排序的链表.py
@@ -0,0 +1,34 @@
+'''
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+限制：
+0 <= 链表长度 <= 1000
+'''
+
+#答：
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+    
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 is not None else l2
+        return prehead.next
